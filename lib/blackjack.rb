9ac180a1eaec5daddccb7da2e4e346f457783fa2
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-06 17:06:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,7 +43,7 @@ def hit? (card_total)
   # code hit? here
   prompt_user()
   get_user_input()
-  
+  user_input = get_user_input()
   #if (get_user_input != 'h' || get_user_input != 's')
    # invalid_command()
   #end 
